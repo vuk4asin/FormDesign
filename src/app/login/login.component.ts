b1274d4e9ee294fb6771606a23a363732a7ce635
vuk4asin <vukasin.tonic@gmail.com>
2022-02-04 10:41:54 +0100

    Dodatak

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
 
 
   username?:string;
+  email?:string;
   password?:number;
 
   constructor(private service:UserService) { }
@@ -27,8 +28,12 @@ export class LoginComponent implements OnInit {
     if(!this.password){
       alert("Please fill password");
     }
+    if(!this.email){
+      alert("Please fill email")
+    }
     const User= {
       username: this.username,
+      email:this.email,
       password: this.password
     }
     console.log(User);
